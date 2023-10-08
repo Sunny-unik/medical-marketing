f2b8756ea43f2b84321f86f961ccb332b3b7390c
Sunny Gandhwani <sunnygandhwani027@gmail.com>
2023-10-08 15:58:29 +0530

    setup prismic repo for development

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { repositoryName } from "@/prismicio";
+import "@/styles/globals.css";
+import { PrismicPreview } from "@prismicio/next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <PrismicPreview repositoryName={repositoryName} />
+    </>
+  );
 }
