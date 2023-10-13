f17801aa2b9288aae05a5e32f1eefb354c1ab64e
templar-ajay <templar.ajay@gmail.com>
2023-10-13 23:53:24 +0530

    fix folder name of images

--- components/sections/navigation/Header.js
+++ components/sections/navigation/Header.js
@@ -10,13 +10,13 @@ const Header = ({ navigationItems }) => {
     <SectionWrapper direction="row" pbs={0}>
       <header className="flex flex-col largeTablet:flex-row flex-grow py-9 font-inter justify-between items-center">
         <DesktopNavigation
-          logo={"/medicosLogo/medicos-logo-trans-wt.png"}
+          logo={"./medicosLogo/medicos-logo-trans-wt.png"}
           navigationItems={navigationItems}
         />
         <div className="flex largeTablet:w-auto w-full">
           {/* // w-full largeTablet:hidden */}
           <MobileNavigation
-            logo={"/medicosLogo/medicos-logo-trans-wt.png"}
+            logo={"./medicosLogo/medicos-logo-trans-wt.png"}
             navigationItems={navigationItems}
           />
         </div>
